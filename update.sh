d6ff912973e998f2b6eaace5e118767138d3cb67
AlanRubigati <56373007+AlanRubigati@users.noreply.github.com>
2023-01-15 01:32:47 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
-echo "testando"
+rm *
+git init
+git config --global user.name "AlanRubigati"
+git config --global user.email "rubigati@hotmail.com"
+git remote add -f origin https://github.com/AlanRubigati/projeto-universal.git
