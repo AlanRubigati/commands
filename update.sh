394d9ba44dc990367150d2a8faae6ebcb196a070
AlanRubigati <56373007+AlanRubigati@users.noreply.github.com>
2023-01-15 04:23:46 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,5 +5,4 @@ cd backend
 sudo dart pub get
 sudo rm bin/backend.exe -f
 sudo dart compile exe bin/backend.dart -o bin/backend.exe
-pm2 delete all -f
-pm2 start bin/backend.exe -f
+pm2 restart all -f
