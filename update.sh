9b3cf52991292f3ba20e30f9c10f210a99e535ca
AlanRubigati <56373007+AlanRubigati@users.noreply.github.com>
2023-01-15 04:13:55 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ cd /app
 sudo git pull origin main
 cd backend
 sudo dart pub get
-sudo rm bin/backend.exe
+sudo rm bin/backend.exe -f
 sudo dart compile exe bin/backend.dart -o bin/backend.exe
 pm2 delete all -f
 pm2 start bin/backend.exe -f
