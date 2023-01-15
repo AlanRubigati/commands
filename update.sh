c8f275a45514c6b49d4b8ee7892972f3d83b5e70
AlanRubigati <56373007+AlanRubigati@users.noreply.github.com>
2023-01-15 02:02:17 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,8 +6,9 @@ sudo apt update
 sudo git init
 sudo git remote add -f origin https://github.com/AlanRubigati/projeto-universal.git
 sudo git config core.sparseCheckout true
-sudo git sparse-checkout set backend.exe
+sudo git sparse-checkout set backend
 sudo git pull origin main
 cd backend
+sudo dart compile exe bin/backend.dart -o bin/backend.exe
 pm2 delete all
 pm2 start bin/backend.exe
